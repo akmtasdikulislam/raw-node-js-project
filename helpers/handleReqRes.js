1a18a61bd5c04dc7cfdbda6b2ba077aa59f45c85
Akm Tasdikul Islam <akmtasdikulislam@gmail.com>
2025-01-25 17:19:42 +0600

    made the post data accessible for chosen handler

--- helpers/handleReqRes.js
+++ helpers/handleReqRes.js
@@ -9,7 +9,7 @@
 // Dependencies
 const url = require("url");
 const { StringDecoder } = require("string_decoder");
-const routes = require("./routes");
+const routes = require("../routes");
 const {
   notFoundHandler,
 } = require("../handlers/routeHandlers/notFoundHandler");
@@ -43,15 +43,6 @@ handler.handleReqRes = (req, res) => {
   const chosenHandler = routes[trimmedPath]
     ? routes[trimmedPath]
     : notFoundHandler;
-  chosenHandler(requestProperties, (statusCode, payload) => {
-    statusCode = typeof statusCode === "number" ? statusCode : 500;
-    payload = typeof payload === "object" ? payload : {};
-    const payloadString = JSON.stringify(payload);
-
-    //   Return the final response
-    res.writeHead(statusCode);
-    res.end(payloadString);
-  });
 
   req.on("data", (buffer) => {
     realData += decoder.write(buffer);
@@ -59,7 +50,16 @@ handler.handleReqRes = (req, res) => {
 
   req.on("end", () => {
     realData += decoder.end();
-    console.log(realData);
+
+    chosenHandler(requestProperties, (statusCode, payload) => {
+      statusCode = typeof statusCode === "number" ? statusCode : 500;
+      payload = typeof payload === "object" ? payload : {};
+      const payloadString = JSON.stringify(payload);
+
+      //   Return the final response
+      res.writeHead(statusCode);
+      res.end(payloadString);
+    });
   });
 };
 

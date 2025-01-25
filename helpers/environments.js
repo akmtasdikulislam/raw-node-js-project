168fb1c22d6b374cfcac0c7fb7873a23dd79945e
Akm Tasdikul Islam <akmtasdikulislam@gmail.com>
2025-01-25 17:18:50 +0600

    set up environment variables for this project

---
+++ helpers/environments.js
@@ -0,0 +1,34 @@
+/*
+ * Title: Environment Variables
+ * Description: Handle all environment variables
+ * Author: Akm Tasdikul Islam
+ * Date: 25/01/2025
+ *
+ */
+// Dependencies
+
+// Module scaffolding
+const environments = {};
+
+environments.staging = {
+  port: 3000,
+  envName: "staging",
+  secretKey: "asdfghjkl",
+};
+environments.production = {
+  port: 5000,
+  envName: "production",
+  secretKey: "asdfghjkl",
+};
+
+// Determine which environment was passed
+const currentEnvironment =
+  typeof process.env.NODE_ENV === "string" ? process.env.NODE_ENV : "staging";
+
+// Export corresponding environment object
+const environmentToExport =
+  typeof environments[currentEnvironment] === "object"
+    ? environments[currentEnvironment]
+    : environments.staging;
+
+module.exports = environmentToExport;
--- index.js
+++ index.js
@@ -9,20 +9,20 @@
 // Dependencies
 const http = require("http");
 const { handleReqRes } = require("./helpers/handleReqRes");
+const environment = require("./helpers/environments");
 
 // App object - module scaffolding
 const app = {};
 
 // Configuration
-app.config = {
-  port: 3000,
-};
+app.config = {};
 
 // Create server
 app.createServer = () => {
   const server = http.createServer(app.handleReqRes);
-  server.listen(app.config.port, () => {
-    console.log(`The server is listening on port ${app.config.port}`);
+  server.listen(environment.port, () => {
+    console.log(`environment variable is ${process.env.NODE_ENV}`);
+    console.log(`The server is listening on port ${environment.port}`);
   });
 };
 
